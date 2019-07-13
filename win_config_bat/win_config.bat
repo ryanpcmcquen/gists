3a6d08c5960f5116420498fee9040f5b7f0a29de
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-07-13 01:46:14 +0000

    Sat Jul 13 01:46:14 UTC 2019
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

---
+++ win_config_bat/win_config.bat
@@ -0,0 +1,7 @@
+:: curl https://gist.githubusercontent.com/ryanpcmcquen/1ccef39e0bb4c75be0d7d834b25bd6bb/raw/win_config.bat -o "%UserProfile%\win_config.bat" && "%UserProfile%\win_config.bat"
+
+curl https://raw.githubusercontent.com/ryanpcmcquen/sublime_witness/master/Witness.sublime-color-scheme -o "%AppData%\Sublime Text 3\Packages\User\Witness.sublime-color-scheme"
+curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/SublimeText/Default.sublime-keymap -o "%AppData%\Sublime Text 3\Packages\User\Default.sublime-keymap"
+curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/SublimeText/Default.sublime-mousemap -o "%AppData%\Sublime Text 3\Packages\User\Default.sublime-mousemap"
+curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/SublimeText/Preferences.sublime-settings -o "%AppData%\Sublime Text 3\Packages\User\Preferences.sublime-settings"
+curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.emacs -o "%AppData%\.emacs"
