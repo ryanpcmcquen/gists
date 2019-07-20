269cd96e31edb0f6d9ec4d9d106f2ae664275c46
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-07-20 21:46:18 +0000

    Sat Jul 20 21:46:18 UTC 2019
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- tHe_js/tHe.js
+++ tHe_js/tHe.js
@@ -1,25 +1,11 @@
 /*! tHe.js v0.2.3 by ryanpcmcquen */
 //
-// Ryan P.C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version, with the following exception:
-// the text of the GPL license may be omitted.
-//
-// This program is distributed in the hope that it will be useful, but
-// without any warranty; without even the implied warranty of
-// merchantability or fitness for a particular purpose. Compiling,
-// interpreting, executing or merely reading the text of the program
-// may result in lapses of consciousness and/or very being, up to and
-// including the end of all existence and the Universe as we know it.
-// See the GNU General Public License for more details.
-//
-// You may have received a copy of the GNU General Public License along
-// with this program (most likely, a file named COPYING).  If not, see
-// <https://www.gnu.org/licenses/>.
-//
+// Ryan P. C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 /*global window*/
 /*jslint browser:true*/
 (function () {
