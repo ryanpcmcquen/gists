c86920086e7f8ed8e6a047c8432d343c150c9de7
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-10-08 16:01:27 +0000

    Tue Oct  8 16:01:27 UTC 2019
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- Funky_configs/hey_dude.sh
+++ Funky_configs/hey_dude.sh
@@ -6,5 +6,5 @@ for dir in $(find ~/repos -maxdepth 1 -type d); do
     git pull --all
     cd
 done
-brew update; brew upgrade; npm update -g; pip install --upgrade pipupgrade; pipupgrade --latest --yes; sudo gem update
+brew update; brew upgrade; npm update -g; pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U; pip install --upgrade pipupgrade; pipupgrade --self; pipupgrade --latest --yes; sudo gem update
 curl -o ~/.osx https://gist.githubusercontent.com/ryanpcmcquen/d0f0a265dde5cfdbd8b49d126083da6a/raw/.osx && bash ~/.osx
