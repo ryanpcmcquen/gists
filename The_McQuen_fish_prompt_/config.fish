6821a31c2e41ad9afa5260f17e1074156e6e3a2d
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2020-01-14 19:46:21 +0000

    Tue Jan 14 19:46:21 UTC 2020
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- The_McQuen_fish_prompt_/config.fish
+++ The_McQuen_fish_prompt_/config.fish
@@ -1,14 +1,13 @@
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-# export PATH="/usr/local/opt/ruby/bin:/usr/local/opt/gnu-tar/libexec/gnubin:/usr/local/sbin:$PATH"
-
 export NODE_EXTRA_CA_CERTS='/usr/local/etc/openssl/cert.pem'
-
-set -gx LDFLAGS "-L/usr/local/opt/node@12/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/node@12/include"
 
 set -U EDITOR subl -w
 set -U VISUAL subl -w
 
 source "$HOME/.config/fish/secrets.fish"
+
+set -gx LDFLAGS "-L/usr/local/opt/node@12/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/node@12/include"
+
 set -g fish_user_paths "/usr/local/opt/node@12/bin" $fish_user_paths
