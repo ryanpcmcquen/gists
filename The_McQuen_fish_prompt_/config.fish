20fd03719dc80aa2c0e94b736b9e229c72c812a4
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2020-01-14 19:01:26 +0000

    Tue Jan 14 19:01:26 UTC 2020
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- The_McQuen_fish_prompt_/config.fish
+++ The_McQuen_fish_prompt_/config.fish
@@ -1,17 +1,14 @@
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-export PATH="/usr/local/opt/ruby/bin:/usr/local/opt/gnu-tar/libexec/gnubin:/usr/local/sbin:$PATH"
-export LDFLAGS="-L/usr/local/opt/ruby/lib"
-export CPPFLAGS="-I/usr/local/opt/ruby/include"
-export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
+# export PATH="/usr/local/opt/ruby/bin:/usr/local/opt/gnu-tar/libexec/gnubin:/usr/local/sbin:$PATH"
 
 export NODE_EXTRA_CA_CERTS='/usr/local/etc/openssl/cert.pem'
 
-export PATH="/usr/local/opt/node.js/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/node.js/lib"
-export CPPFLAGS="-I/usr/local/opt/node.js/include"
+set -gx LDFLAGS "-L/usr/local/opt/node@12/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/node@12/include"
 
 set -U EDITOR subl -w
 set -U VISUAL subl -w
 
 source "$HOME/.config/fish/secrets.fish"
+set -g fish_user_paths "/usr/local/opt/node@12/bin" $fish_user_paths
