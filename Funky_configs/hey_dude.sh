89071dbdcd2317ff740049903d9a21fedaa8baa7
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2020-02-11 17:46:35 +0000

    Tue Feb 11 17:46:35 UTC 2020
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- Funky_configs/hey_dude.sh
+++ Funky_configs/hey_dude.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 wget -N https://gist.githubusercontent.com/ryanpcmcquen/d0f0a265dde5cfdbd8b49d126083da6a/raw/hey_dude.sh -P /usr/local/bin/ && chmod +x /usr/local/bin/hey_dude.sh
-for dir in $(find ~/repos -maxdepth 1 -type d); do
+for dir in $(find ~/code -maxdepth 1 -type d); do
     cd $dir
     echo $dir
     git pull --all
